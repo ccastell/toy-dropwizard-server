c5fde3eaedae6f3b4e44a1da21ac7acd3e513f6b
carlcastello <ccastell@ualberta.ca>
2020-01-27 18:05:14 -0700

    Created two more test

--- test_ci.py
+++ test_ci.py
@@ -1,10 +1,18 @@
 import requests
 import pytest # have to install via command "pip3 install -U pytest"
+from uuid import uuid4
 
 def test_health_check():
     r = requests.get('http://localhost:8085')
     assert(r.status_code == 404)
 
-# 1. Add two more tests here of your choice. I will explain the API.
-#    Make sure to verify the necessary info, e.g., status code, response data.
-# 2. Add "pytest" as integration testing script as part of Github Actions CI workflow (you've done this before!)
+def test_name_input():
+    name = str(uuid4())
+    r = requests.get('http://localhost:8085/hello-world?name={name}'.format(name=name))
+    assert('Hello, {name}!'.format(name=name) == r.json().get('content'))
+
+def test_id_increment():
+    r = requests.get('http://localhost:8085/hello-world?name={name}'.format(name=str(uuid4())))
+    previous_id = r.json().get('id')
+    r = requests.get('http://localhost:8085/hello-world?name={name}'.format(name=str(uuid4())))
+    assert( previous_id + 1 == r.json().get('id'))
